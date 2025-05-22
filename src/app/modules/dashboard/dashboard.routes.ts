e689f7334ed9bcabd5f2ed97b91ea463e1475eed
Guillaume <guillaume.durandiere@ouidou.fr>
2025-05-22 14:03:40 +0200

    feat/init-dashboard :sparkles: init module

---
+++ src/app/modules/dashboard/dashboard.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { DashboardLandingComponent } from './page/dashboard-landing/dashboard-landing.component';
+
+export const dashboardRoutes: Routes = [
+  { path: '', component: DashboardLandingComponent },
+];
