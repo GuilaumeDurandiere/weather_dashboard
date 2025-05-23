062b952133e19f2cacae4eeb319f7f4d8962f4db
Guillaume <guillaume.durandiere@ouidou.fr>
2025-05-23 09:38:06 +0200

    feat/init-dashboard :sparkles: add component and models

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-
   {
     path: 'dashboard',
     loadChildren: () => import('./modules/dashboard/dashboard.routes').then((m) => m.dashboardRoutes),
-    title: ':@@LOGIN:Identification',
   },
-
 ];
---
+++ src/app/core/models/city.ts
@@ -0,0 +1,6 @@
+export interface City {
+  country: string;
+  city: string;
+  lat: number;
+  long: number;
+}
